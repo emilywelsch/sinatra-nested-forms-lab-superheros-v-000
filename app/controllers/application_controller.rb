ca8a1f835a0c196b702b5eaf0202f98ee2576a2d
Emily Welsch <emwelsch@gmail.com>
2018-08-06 21:31:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-          erb :'app/views/index'
+          erb :index
         end
 
     post "/teams" do
