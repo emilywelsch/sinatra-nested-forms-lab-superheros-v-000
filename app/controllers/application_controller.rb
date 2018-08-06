b9a41e39d5fb14e183d5e344e2a1b81d67a0377d
Emily Welsch <emwelsch@gmail.com>
2018-08-06 21:29:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-          erb :'/app/views/index'
+          erb :/app/views/index
         end
 
     post "/teams" do
@@ -14,7 +14,7 @@ class App < Sinatra::Base
         @superheroes = members.collect do |member_params|
           SuperHero.new({name: member_params[:name], power: member_params[:power], bio: member_params[:bio]})
         end
-        erb :'/app/views/team'
+        erb :/app/views/team
     end
 
 end
